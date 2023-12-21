5ef381d73c7bfbb7a927636983c420cc7534fad8
HarukiOkamoto <alice.159357@icloud.com>
2023-12-22 00:15:35 +0900

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
       title: 'らくらく弁当',
       meta: [
         { hid: 'color-scheme', name: 'color-scheme', content: 'light' },
+        { hid: 'google-site-verification', name: 'google-site-verification', content: 'thxSa_fGBDqmwFo20mgOt3DwxpNHaFrMPYRb9X5Ffp8' },
+        { hid: 'description', name: 'description', content: '蕨市で宅配弁当をお探しですか？らくらく弁当は、美味しくて手軽なお弁当を提供しています。' },
+        { hid: 'keywords', name: 'keywords', content: '蕨市,弁当,宅配弁当,らくらく,らくらく弁当,埼玉県,埼玉県蕨市' },
       ],
       link: [
         {
